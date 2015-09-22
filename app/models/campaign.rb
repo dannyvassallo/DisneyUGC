c9276ac3cc63cbd0a17fd024887efeba44a384fc
dannyvassallo <danielvassallo87@gmail.com>
2015-09-22 15:27:15 -0400

    setup carrierwave and fog

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,5 @@
 class Campaign < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :title, use: :slugged
+	mount_uploader :feature, FeatureUploader
 end
--- app/uploaders/feature_uploader.rb
+++ app/uploaders/feature_uploader.rb
@@ -4,11 +4,11 @@ class FeatureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -27,6 +27,7 @@ class FeatureUploader < CarrierWave::Uploader::Base
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
+  process :resize_to_fill => [640, 640]
   # def scale(width, height)
   #   # do something
   # end
@@ -38,9 +39,9 @@ class FeatureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
