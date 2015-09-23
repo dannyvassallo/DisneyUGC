d4af945b3257b64a9a414b2a70230f58e62ce679
dannyvassallo <danielvassallo87@gmail.com>
2015-09-23 11:35:26 -0400

    setup delayed job, carrierwave-backgrounder, and foreman

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,6 +3,8 @@ class Campaign < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :title, use: :slugged
 	mount_uploader :feature, FeatureUploader
+	process_in_background :feature
 	mount_uploader :video, VideoUploader	 
+	process_in_background :video
 
 end
