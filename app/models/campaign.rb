5e04601105bf0b696594002c9c5a212dbfa0c866
dannyvassallo <danielvassallo87@gmail.com>
2015-09-24 12:03:15 -0400

    added validation to campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -6,5 +6,7 @@ class Campaign < ActiveRecord::Base
 	process_in_background :feature
 	mount_uploader :video, VideoUploader	 
 	process_in_background :video
-
+	validates_presence_of :title
+	validates_presence_of :description
+	validates_presence_of :call_to_action
 end
