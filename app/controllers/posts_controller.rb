fab079d3e86c66c763b12b056840971f3767152d
dannyvassallo <danielvassallo87@gmail.com>
2015-09-24 14:49:40 -0400

    formatted posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,65 @@
 class PostsController < ApplicationController
-  
+  respond_to :html, :js
+
   def index
-  end
-
-  def edit
-  end
-
-  def update
+    @posts = Post.all
   end
 
   def show
-  end
-
-  def create
+    @post = Post.friendly.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    title = @post.title
+
+    if @post.save
+      flash[:notice] = "Your new post was created!"
+      redirect_to posts_path
+    else
+      flash[:error] = "There was an error creating the post. Please try again."
+      render :new
+    end 
+  end
+
+  def edit
+    @post = Post.friendly.find(params[:id])
+  end
+
+  def update
+    @post = Post.friendly.find(params[:id])
+    title = @post.title
+
+    if @post.update_attributes(post_params)
+      flash[:notice] = "The post was updated!"
+      redirect_to posts_path
+    else
+      flash[:error] = "There was an error updating the post. Please try again."
+      render :edit
+    end    
   end
 
   def destroy
+    @post = Post.friendly.find(params[:id])
+    title = @post.title
+
+    if @post.destroy
+      flash[:notice] = "The post was deleted successfully."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error deleting the post. Please try again."
+      render :show
+    end    
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image_url, :video_url)
   end
 
 end
