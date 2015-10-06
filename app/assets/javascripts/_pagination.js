e98630cd1282eeff9d87f51a182d222e9a89e994
dannyvassallo <danielvassallo87@gmail.com>
2015-10-06 11:43:03 -0400

    removed old code

--- app/assets/javascripts/_pagination.js
+++ app/assets/javascripts/_pagination.js
@@ -3,8 +3,7 @@ $(function() {
     $.getScript(this.href).done(function(){    	
 		$('video').each(function(){
 			videojs(this);
-		});
-    	$.each(videojs.players, function (key, player) { if (player.isReady) { player.destroy(); } else { delete videojs.players[player.id]; } });
+		});    	
     });
     return false;
   });  
