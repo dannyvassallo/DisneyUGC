e3dfdf08cca7d05c90e1f6304a3f1350f71fd3fd
dannyvassallo <danielvassallo87@gmail.com>
2015-10-07 14:32:06 -0400

    upped thumb sizes and fixed duplicate form entry modal overlays

--- app/assets/javascripts/_initialize-materialize.js
+++ app/assets/javascripts/_initialize-materialize.js
@@ -2,7 +2,7 @@ $(function(){
 	$('.button-collapse').sideNav();
 	$('.slider').slider({full_width: true});
 	$('.parallax').parallax();
-	$('.modal-trigger').leanModal({
+	$('.feed-modal-trigger').leanModal({
 		ready: function() { 
 			// pause featured video on modal open
 			$('.primary-player video')[0].player.pause();
@@ -11,9 +11,10 @@ $(function(){
 			// Pause this video on modal close
 		    $('.feed-vid').each(function(){ 
 		      this.player.pause(); 
-		    });
+		    });		    
 		} 
 	});
+	$('.modal-trigger').leanModal();
 	$('.datepicker').pickadate({
 		selectMonths: true, // Creates a dropdown to control month
 		selectYears: 100, // Creates a dropdown of 15 years to control year
