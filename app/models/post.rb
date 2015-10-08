611c8b48416f47e52a15b77b39f717fc5213d65a
dannyvassallo <danielvassallo87@gmail.com>
2015-10-08 16:37:29 -0400

    added age validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
 	validates_presence_of :email_address
 	validates_presence_of :dob
 	validate :image_or_video
+	validate :age
 
     #other properties 
 	belongs_to :campaign
@@ -18,5 +19,13 @@ class Post < ActiveRecord::Base
 			errors.add(:image_url, "Can't be blank")
 			errors.add(:video_url, "Can't be blank")
 		end
+	end
+
+	# age
+	def age
+	  user_age = (Time.now.to_s(:number).to_i - dob.to_time.to_s(:number).to_i)/10e9.to_i
+	  if user_age < 13
+	  	errors.add(:dob, "Sorry, you aren't old enough to use this site.")
+	  end
 	end
 end
