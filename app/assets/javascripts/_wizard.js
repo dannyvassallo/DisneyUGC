d616c7c4f276a2a02979a25e43687964fdfda29d
dannyvassallo <danielvassallo87@gmail.com>
2015-10-08 13:30:52 -0400

    replaced js alerts with toast messages from materialize

--- app/assets/javascripts/_wizard.js
+++ app/assets/javascripts/_wizard.js
@@ -16,21 +16,25 @@ $('.the-wizard').stepy({
 	description: false,
 	transition: 'fade',
 	next: function(index) {	
+		// VALIDATION ON NEXT
 		// field variables
 		var field = $(this).find('input[type=file],input[type=email],input[type=text],textarea,select').filter(':visible:first'),
 		fieldId = field.attr('id'),
 		fieldLabel = field.prev('label').html().toLowerCase(),
 		fieldValue = field.val(),
+		errorMsg = '';		
 		// email variables
 		emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;		
 		// Validation on next
 	    if (fieldValue.length <= 2){
-	    	// field length validation
-	    	alert('Your '+fieldLabel+'\'s must be longer than that.');
+	    	// field length validation	    	
+			errorMsg = 'Your '+fieldLabel+' must be longer than that.';
+			Materialize.toast(errorMsg, 5000, 'red darken-4');
 	    	return false;
     	// Email validation
 	    } else if (fieldLabel == 'email address' && !emailRegex.test(fieldValue)){
-	    	alert('Your '+fieldLabel+' must be valid.');
+	    	errorMsg = 'Your '+fieldLabel+' must be valid.';
+			Materialize.toast(errorMsg, 5000, 'red darken-4');
 	    	return false;
     	// Validate Age
 	    } else if (fieldLabel == 'date of birth'){
@@ -38,7 +42,8 @@ $('.the-wizard').stepy({
 	    	var bDay = +new Date(bDate);
     	    var age = (Date.now() - bDay) / (31557600000);
 		    if ( age < 13 ) {
-		        alert('Sorry, but you are not eligible for this sweepstakes.');
+		        errorMsg = 'Sorry, but you are not eligible for this sweepstakes.';
+    			Materialize.toast(errorMsg, 5000, 'red darken-4');
 		        return false;
 		    }
 	    }
