3124d7b125cc67bc51e3db1e399a65de8c74e89d
dannyvassallo <danielvassallo87@gmail.com>
2015-10-08 16:29:49 -0400

    setup server side validation for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,22 @@
 class Post < ActiveRecord::Base
+	# server side validation	
+	validates_presence_of :full_name
+	validates_presence_of :email_address
+	validates_presence_of :dob
+	validate :image_or_video
+
+    #other properties 
 	belongs_to :campaign
 	mount_uploader :image_url, Image_urlUploader
 	process_in_background :image_url	
 	mount_uploader :video_url, Video_urlUploader
 	process_in_background :video_url	
+
+	# image or video
+	def image_or_video
+		if image_url.blank? && video_url.blank?
+			errors.add(:image_url, "Can't be blank")
+			errors.add(:video_url, "Can't be blank")
+		end
+	end
 end
