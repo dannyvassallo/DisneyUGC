4940ec7d14b476db8d148f6cb8cdc2749ae872c6
dannyvassallo <danielvassallo87@gmail.com>
2015-10-08 13:11:06 -0400

    added name, email, and DOB validation

--- app/assets/javascripts/_wizard.js
+++ app/assets/javascripts/_wizard.js
@@ -16,13 +16,32 @@ $('.the-wizard').stepy({
 	description: false,
 	transition: 'fade',
 	next: function(index) {	
+		// field variables
 		var field = $(this).find('input[type=file],input[type=email],input[type=text],textarea,select').filter(':visible:first'),
 		fieldId = field.attr('id'),
-		fieldValue = field.val();			
+		fieldLabel = field.prev('label').html().toLowerCase(),
+		fieldValue = field.val(),
+		// email variables
+		emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;		
+		// Validation on next
 	    if (fieldValue.length <= 1){
-	    	alert('NO WAY');
+	    	// field length validation
+	    	alert('Your '+fieldLabel+' must be longer than that.');
 	    	return false;
-	    } 
+    	// Email validation
+	    } else if (fieldLabel == 'email address' && !emailRegex.test(fieldValue)){
+	    	alert('Your '+fieldLabel+' must be valid.');
+	    	return false;
+    	// Validate Age
+	    } else if (fieldLabel == 'date of birth'){
+	    	var bDate = fieldValue;
+	    	var bDay = +new Date(bDate);
+    	    var age = (Date.now() - bDay) / (31557600000);
+		    if ( age < 13 ) {
+		        alert('Sorry, but you are not eligible for this sweepstakes.');
+		        return false;
+		    }
+	    }
 	}
 });					
 
