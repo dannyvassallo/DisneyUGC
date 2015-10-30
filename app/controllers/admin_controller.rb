f2643b1861701c8826d6592c706e7c4ddfdc71c7
dannyvassallo <danielvassallo87@gmail.com>
2015-10-30 12:12:07 -0400

    added search and pagination to admin panel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,8 +4,14 @@ class AdminController < ApplicationController
 
   def index
     @user = current_user    
+    results_per_page = 20
     if user_admin(@user)
-      @users = User.all
+      @users = User.all.paginate(:page => params[:page], :per_page => results_per_page).order('created_at DESC')  
+      if params[:search]
+        @users = User.search(params[:search]).paginate(:page => params[:page], :per_page => results_per_page).order('created_at DESC')        
+      else
+        @users = User.all.order("created_at DESC").paginate(:page => params[:page], :per_page => results_per_page).order('created_at DESC')  
+      end      
     else
       not_found
     end        
