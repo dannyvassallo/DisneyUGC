8ec8bd3c065f0e486625929e765ab089209ff6f4
dannyvassallo <danielvassallo87@gmail.com>
2015-11-05 15:36:51 -0500

    added tests for all campaign views

--- spec/views/campaign_views_spec.rb
+++ spec/views/campaign_views_spec.rb
@@ -29,6 +29,17 @@ describe "Campaign Security Tests" do
     end
   end
 
+  describe "Anon tries to visit a dead campaign" do
+    before do
+      logout(@user)
+    end
+    it "shows Anon the error page" do                
+      visit campaign_path(@dead_campaign)
+      expect(@dead_campaign).to render_template(:partial =>'campaigns/_error')
+      print ("\nAnon was not shown the dead campaign")
+    end
+  end
+
   describe "Admin user tries to visit a dead campaign" do
     before do      
       @user.update_attributes(:role => 'admin')
@@ -49,6 +60,17 @@ describe "Campaign Security Tests" do
     end
   end
 
+  describe "Anon visits live campaign" do
+    before do
+      logout(@user)
+    end  
+    it "shows Anon the campaign without admin tools" do
+      visit campaign_path(@live_campaign)
+      expect(page).to_not have_css('#admin-tools')
+      print ("\nAnon couldn't see admin tools on a live campaign")
+    end
+  end  
+
   describe "Admin user visits live campaign" do
     before do      
       @user.update_attributes(:role => 'admin')
@@ -58,6 +80,100 @@ describe "Campaign Security Tests" do
       visit campaign_path(@live_campaign)
       expect(page).to have_css('#admin-tools')
       print ("\nAdmin could see admin tools on a live campaign")
+    end
+  end
+
+  describe "Non-admin user tries to visit campaign index" do
+    it "shows the user the error page" do                
+      visit campaigns_path
+      expect(campaigns_path).to render_template(:partial =>'campaigns/_error')
+      print ("\nUser was not shown the campaign index")
+    end
+  end
+
+  describe "Anon tries to visit campaign index" do
+    before do
+      logout(@user)
+    end
+    it "shows Anon the error page" do                
+      visit campaigns_path
+      expect(campaigns_path).to render_template(:partial =>'campaigns/_error')
+      print ("\nAnon was not shown the campaign index")
+    end
+  end
+
+  describe "Admin user tries to visit campaign index" do
+    before do      
+      @user.update_attributes(:role => 'admin')
+      @user.save!
+    end
+    it "shows the admin the campaign index" do
+      visit campaigns_path
+      expect(campaigns_path).to_not render_template(:partial =>'campaigns/_error')
+      expect(campaigns_path).to render_template(:partial =>'campaigns/_campaign')
+      print ("\nAdmin was shown the campaign index")
+    end
+  end
+
+  describe "Non-admin user tries to visit new campaign path" do
+    it "shows the user the error page" do                
+      visit new_campaign_path
+      expect(new_campaign_path).to render_template(:partial =>'campaigns/_error')
+      print ("\nUser was not shown the new campaign path")
+    end
+  end
+
+  describe "Anon tries to visit new campaign path" do
+    before do
+      logout(@user)
+    end
+    it "shows Anon the error page" do                
+      visit new_campaign_path
+      expect(new_campaign_path).to render_template(:partial =>'campaigns/_error')
+      print ("\nAnon was not shown the new campaign path")
+    end
+  end
+
+  describe "Admin user tries to visit new campaign path" do
+    before do      
+      @user.update_attributes(:role => 'admin')
+      @user.save!
+    end
+    it "shows the admin the campaign new campaign path" do
+      visit new_campaign_path
+      expect(new_campaign_path).to_not render_template(:partial =>'campaigns/_error')      
+      print ("\nAdmin was shown the new campaign path")
+    end
+  end
+
+  describe "Non-admin user tries to visit edit campaign path" do
+    it "shows the user the error page" do                
+      visit edit_campaign_path(@live_campaign)
+      expect(edit_campaign_path(@live_campaign)).to render_template(:partial =>'campaigns/_error')
+      print ("\nUser was not shown the edit campaign path")
+    end
+  end
+
+  describe "Anon tries to visit edit campaign path" do
+    before do
+      logout(@user)
+    end
+    it "shows Anon the error page" do                
+      visit edit_campaign_path(@live_campaign)
+      expect(edit_campaign_path(@live_campaign)).to render_template(:partial =>'campaigns/_error')
+      print ("\nAnon was not shown the edit campaign path")
+    end
+  end
+
+  describe "Admin user tries to visit edit campaign path" do
+    before do      
+      @user.update_attributes(:role => 'admin')
+      @user.save!
+    end
+    it "shows the admin the edit campaign path" do
+      visit edit_campaign_path(@live_campaign)
+      expect(edit_campaign_path(@live_campaign)).to_not render_template(:partial =>'campaigns/_error')      
+      print ("\nAdmin was shown the edit campaign path")
     end
   end
 
