13df6f385593a393032b2965af42a6d11639c2f5
dannyvassallo <danielvassallo87@gmail.com>
2015-11-16 17:14:21 -0500

    fixed video thumbnails to crop to square altered seeds and logged duration for future length validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,39 +1,51 @@
+require 'streamio-ffmpeg'
+require 'mini_magick'
+
 class Post < ActiveRecord::Base
-	# server side validation	
-	validates_presence_of :full_name
-	validates_presence_of :email_address
-	validates_presence_of :dob
-	validate :image_or_video
-	validate :age
+  # server side validation  
+  validates_presence_of :full_name
+  validates_presence_of :email_address
+  validates_presence_of :dob
+  validate :image_or_video
+  validate :video
+  validate :age 
 
     #other properties 
-	belongs_to :campaign
-	mount_uploader :image_url, Image_urlUploader
-	process_in_background :image_url	
-	mount_uploader :video_url, Video_urlUploader
-	process_in_background :video_url	
+  belongs_to :campaign
+  mount_uploader :image_url, Image_urlUploader
+  process_in_background :image_url  
+  mount_uploader :video_url, Video_urlUploader
+  process_in_background :video_url  
 
-	# image or video
-	def image_or_video
-		if image_url.blank? && video_url.blank?
-			errors.add(:image_url, "Can't be blank")
-			errors.add(:video_url, "Can't be blank")
-		end
-	end
+  # image or video
+  def image_or_video
+    if image_url.blank? && video_url.blank?
+      errors.add(:image_url, "Can't be blank")
+      errors.add(:video_url, "Can't be blank")
+    end
+  end
 
-	# age
-	def age				
-		if dob.blank?
-			errors.add(:dob, "Can't be blank.")		
-		else
-			user_age = (Time.now.to_s(:number).to_i - dob.to_time.to_s(:number).to_i)/10e9.to_i
-			if user_age < 13				
-				errors.add(:dob, "Sorry, you aren't old enough to use this site.")		
-			end
-		end
-	end
+  #validates length
+  def video
+    if !video_url.blank?
+      movie = FFMPEG::Movie.new(self.video_url.path)      
+      print "THE LENGTH IS #{movie.duration}"
+    end
+  end
 
-	# to csv
+  # age
+  def age       
+    if dob.blank?
+      errors.add(:dob, "Can't be blank.")   
+    else
+      user_age = (Time.now.to_s(:number).to_i - dob.to_time.to_s(:number).to_i)/10e9.to_i
+      if user_age < 13        
+        errors.add(:dob, "Sorry, you aren't old enough to use this site.")    
+      end
+    end
+  end
+
+  # to csv
   def self.to_csv
     attributes = %w{created_at full_name email_address image_path video_path}
 
@@ -46,16 +58,17 @@ class Post < ActiveRecord::Base
     end
   end
 
-  def image_path  	
-		self.image_url.url
+  def image_path    
+    self.image_url.url
   end
 
   def video_path
-		self.video_url.url
+    self.video_url.url
   end  
 
   def self.search(search)
     where("(full_name || ' ' || email_address) LIKE ?", "%#{search.downcase}%")     
   end
+
 
 end
--- app/uploaders/video_url_uploader.rb
+++ app/uploaders/video_url_uploader.rb
@@ -3,35 +3,51 @@ require 'streamio-ffmpeg'
 
 class Video_urlUploader < CarrierWave::Uploader::Base
   
-	include CarrierWave::Video	
-	include CarrierWave::Video::Thumbnailer
-	include ::CarrierWave::Backgrounder::Delay
-	include CarrierWave::MiniMagick
+  include CarrierWave::Video  
+  include CarrierWave::Video::Thumbnailer
+  include ::CarrierWave::Backgrounder::Delay
+  include CarrierWave::MiniMagick
 
-	storage :fog
+  storage :fog
 
-	process :encode_video=> [:mp4, audio_codec: "aac",:custom => "-strict experimental -q:v 5 -preset slow -g 30"]
+  process :encode_video=> [:mp4, audio_codec: "aac",:custom => "-strict experimental -q:v 5 -preset slow -g 30"]
 
-	version :thumb do
-		process thumbnail: [{format: 'png', quality: 10, size: 320, strip: false, logger: Rails.logger}]
-		process :resize_to_fill => [320, 320]
-		def full_filename for_file
-			png_name for_file, version_name
-		end
-	end
+  version :thumb do
+    
+    process thumbnail: [{format: 'png', quality: 10, size: 320, strip: false, logger: Rails.logger}]    
+    process resize_to_fill: [320, 320]
+    process :efficient_conversion => [320, 320]
+    
+    def full_filename for_file      
+      png_name for_file, version_name
+    end
 
-	def png_name for_file, version_name
-		%Q{#{version_name}_#{for_file.chomp(File.extname(for_file))}.png}
-	end
+    def png_name for_file, version_name
+      %Q{#{version_name}_#{for_file.chomp(File.extname(for_file))}.png}
+    end
 
-	# Override the directory where uploaded files will be stored.
-	# This is a sensible default for uploaders that are meant to be mounted:
-	def store_dir
-		"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-	end
+    def efficient_conversion(width, height)
+      manipulate! do |img|
+        img.format("png") do |c|
+          c.fuzz        "3%"
+          c.trim
+          c.resize      "#{width}x#{height}>"
+          c.resize      "#{width}x#{height}<"
+        end
+        img
+      end
+    end    
 
-	def extension_white_list
-		%w(ogg ogv 3gp mp4 m4v webm mov m2v 3g2)    
-	end
+  end
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(ogg ogv 3gp mp4 m4v webm mov m2v 3g2)    
+  end
 
 end
