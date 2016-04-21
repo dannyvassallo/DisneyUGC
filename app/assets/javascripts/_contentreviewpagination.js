1c302bc0e5454a9c5331171b28797dcec7c2a07e
dannyvassallo <danielvassallo87@gmail.com>
2016-04-21 11:37:12 -0400

    reinitialize masonry on paginate

--- app/assets/javascripts/_contentreviewpagination.js
+++ app/assets/javascripts/_contentreviewpagination.js
@@ -1,19 +1,10 @@
 $(function() {
   $(".sort_paginate_ajax").on("click", ".pagination a", function(){
     setTimeout(function(){
-      // $("img.lazy").lazyload();
-      if ($('#helpers:checkbox:checked').val() === "on"){
-        $('*').each(function(){
-          if (typeof $(this).data("tooltip") === "undefined"){
-            // Do NOTHING Element is not Tooltipped
-          } else if ($(this).data().hasOwnProperty("tooltip") && !$(this).hasClass("tooltipped")) {
-            // Add The tooltipped class
-            $(this).addClass("tooltipped");
-            // reinitialize tooltips
-            $(this).tooltip({delay: 50});
-          }
-        });
-      }
+      forceBrowserRepaint();
+      $container.imagesLoaded().progress( function() {
+        $container.masonry('layout');
+      });
     }, 50);
     $('.feed-vid').each(function(){
   		videojs(this).dispose();
