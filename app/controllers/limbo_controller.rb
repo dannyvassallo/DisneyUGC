401690a0ee6803565f68b1fd57cd5128f9371b5c
dannyvassallo <danielvassallo87@gmail.com>
2016-06-09 13:47:12 -0400

    limbo redirects depending on role

--- app/controllers/limbo_controller.rb
+++ app/controllers/limbo_controller.rb
@@ -1,6 +1,12 @@
 class LimboController < ApplicationController
 
   def index
+    @user = current_user
+    if @user.admin?
+      redirect_to root_url
+    elsif @user.reviewer?
+      redirect_to practices_review_index_path
+    end
   end
 
 end
