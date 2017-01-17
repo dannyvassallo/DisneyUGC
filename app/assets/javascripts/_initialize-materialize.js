7de0ef5a3b57a4386fe4d5e562cb10ddb1cd999a
mqschwanda <mqschwanda@gmail.com>
2017-01-17 15:33:59 -0500

    materialize date picker implementation

--- app/assets/javascripts/_initialize-materialize.js
+++ app/assets/javascripts/_initialize-materialize.js
@@ -15,10 +15,51 @@ $(function(){
 		}
 	});
 	$('.modal-trigger').leanModal();
-	// $('.datepicker').pickadate({
-	// 	selectMonths: true, // Creates a dropdown to control month
-	// 	selectYears: 100, // Creates a dropdown of 15 years to control year
-	// 	container: 'body',
-	// });
+	$('.datepicker').pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		showMonthsShort: true, // abreviate months
+		selectYears: 100, // Creates a dropdown of 15 years to control year
+		container: 'body',
+		disable_picker: true
+	});
+
+	function selectDays(){
+		$('<select class="picker__select--month browser-default" aria-controls="post_dob_table" title="Select a day">'+
+			 	 '<option value="1">1</option>'+
+			 	 '<option value="2">2</option>'+
+			 	 '<option value="3">3</option>'+
+			 	 '<option value="4">4</option>'+
+			 	 '<option value="5">5</option>'+
+			 	 '<option value="6">6</option>'+
+			 	 '<option value="7">7</option>'+
+			 	 '<option value="8">8</option>'+
+			 	 '<option value="9">9</option>'+
+			 	 '<option value="10">10</option>'+
+			 	 '<option value="11">11</option>'+
+				 '<option value="12">12</option>'+
+				 '<option value="13">13</option>'+
+				 '<option value="14">14</option>'+
+				 '<option value="15">15</option>'+
+				 '<option value="16">16</option>'+
+				 '<option value="17">17</option>'+
+				 '<option value="18">18</option>'+
+				 '<option value="19">19</option>'+
+				 '<option value="20">20</option>'+
+				 '<option value="21">21</option>'+
+				 '<option value="22">22</option>'+
+				 '<option value="23">23</option>'+
+				 '<option value="24">24</option>'+
+				 '<option value="25">25</option>'+
+				 '<option value="26">26</option>'+
+				 '<option value="27">27</option>'+
+				 '<option value="28">28</option>'+
+				 '<option value="29">29</option>'+
+				 '<option value="30">30</option>'+
+				 '<option value="31">31</option>'+
+			 '</select>').insertBefore('.picker__select--month');
+	}
+	selectDays();
+
+
   $('.parallax').parallax();
 });
---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,41 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require zeroclipboard
+//= require materialize-sprockets
+//= require video
+//= require trix
+//= require _pagination
+//= require _jquery.stepy
+//= require _wizard
+//= require _imgloader
+//= require _video-resizer
+//= require _copy-url
+//= require _slideshowresize
+//= require _helpers
+//= require _disablemediatype
+//= require jquery.minicolors
+//= require _shadow
+//= require _imagesloaded
+//= require _initmasonry
+//= require _masonry.pkgd.min
+//= require _content-review
+//= require _jquery.appear
+//= require _appearinit
+//= require _jquery.lazyload
+//= require _lazyloadinit
+//= require _hammer
+//= require _jquery.hammer
+//= require _DateTimePicker
+//= require_tree .
